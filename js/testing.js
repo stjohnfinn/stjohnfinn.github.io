79632aad4506603379c3ce511ee6f1c45eeef254
stjohnfinn <stjohn.finn@gmail.com>
2021-02-10 22:20:18 -0800

    -

--- js/testing.js
+++ js/testing.js
@@ -7,6 +7,6 @@ $(document).ready( () => {
         fetch('https://us-central1-silver-script-302408.cloudfunctions.net/getHTMLContent?url=http://example.com')
             .then(response => response.json() )
             .then(data => { console.log(data) } )
-            .catch( error => {console.log(error)} );
+            .catch( error => { console.log(error) } );
     });
 });
