323a432c87ab7333280607f41dfdd6d0b8c54859
stjohnfinn <stjohn.finn@gmail.com>
2021-02-10 22:27:16 -0800

    -

--- js/testing.js
+++ js/testing.js
@@ -3,9 +3,9 @@ $(document).ready( () => {
     $('#btnTest').click( () => {
         console.log('launching test...');
 
-        // fetch('https://us-central1-silver-script-302408.cloudfunctions.net/getHTMLContent?url=http://example.com')
-        //     .then(response => response.json() )
-        //     .then(data => { console.log(data) } )
-        //     .catch( error => { console.log(error) } );
+        fetch('https://us-central1-silver-script-302408.cloudfunctions.net/getHTMLContent?url=http://example.com')
+            .then(response => response.json() )
+            .then(data => { console.log(data) } )
+            .catch( error => { console.log(error) } );
     });
 });
