d0e6e020e647cadb051a86509aefe5e4194fefd5
stjohnfinn <stjohn.finn@gmail.com>
2021-02-10 22:14:55 -0800

    -

--- js/testing.js
+++ js/testing.js
@@ -1,3 +1,10 @@
 $(document).ready( () => {
     console.log('Testing');
+
+    $('#btnTest').click( () => {
+        fetch('https://us-central1-silver-script-302408.cloudfunctions.net/getHTMLContent?url=http://example.com')
+            .then(response => response.json() )
+            .then(data => console.log(data) )
+            .catch( error => console.log(error) );
+    });
 });
